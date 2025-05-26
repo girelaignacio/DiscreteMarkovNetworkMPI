b667cc5ee50571b6c4b845614a0421c1452c64a5
girelaignacio <girelaignacio@gmail.com>
2025-05-26 18:19:30 -0300

    sims

--- code/simulations/simulations.py
+++ code/simulations/simulations.py
@@ -1,33 +1,34 @@
 import os
+print(os.getcwd())
+
 import numpy as np
 import pandas as pd
 from discrete_gm_nonpos import discrete_graphical_model
-print(os.getcwd())
 
 ncores = 40
-for n_sim in range(1,6):
+for n_sim in range(2,6):
+    print("Estimating SIMULATION NUMBER ",str(n_sim))
 
-    data = np.genfromtxt("./code/sim"+str(n_sim)+"_data.txt", delimiter=' ')
+    data = np.genfromtxt("./code/simulations/sim"+str(n_sim)+"_data.txt", delimiter=' ')
 
 
     print("estimating unstable...")
     ci = discrete_graphical_model(np.linspace(.1,3,2), ncores = ncores).estimate_CI(data)
-    print(ci)
     conserv = ci['conserv']
-    np.savetxt("./code/sim1_conserv_c0.1.txt", 
+    np.savetxt("./code/simulations/sim"+str(n_sim)+"_conserv_c0.1.txt", 
                        conserv[0,:,:], fmt="%5i")
-    np.savetxt("./code/sim1_conserv_c3.txt", 
+    np.savetxt("./code/simulations/sim"+str(n_sim)+"_conserv_c3.txt", 
                        conserv[1,:,:], fmt="%5i")
     nconserv = ci['nconserv']
-    np.savetxt("./code/sim1_nconserv_c0.1.txt", 
+    np.savetxt("./code/simulations/sim"+str(n_sim)+"_nconserv_c0.1.txt", 
                        nconserv[0,:,:], fmt="%5i")
-    np.savetxt("./code/sim1_nconserv_c3.txt", 
+    np.savetxt("./code/simulations/sim"+str(n_sim)+"_nconserv_c3.txt", 
                        nconserv[1,:,:], fmt="%5i")
 
-    print("estimating stable..."
+    print("estimating stable...")
     ci_stable = discrete_graphical_model(np.geomspace(.1, 10,10000),ncores= ncores).estimate_stable_CI(
                                 data, PFER=1., pi_min = 0.6, pi_max=0.9, npartitions = 50)
-    np.savetxt("./code/sim1_stable_conserv.txt", 
+    np.savetxt("./code/simulations/sim"+str(n_sim)+"_stable_conserv.txt", 
                        ci_stable['conserv'], fmt="%5i")
-    np.savetxt("./code/sim1_stable_nconserv.txt", 
+    np.savetxt("./code/simulations/sim"+str(n_sim)+"_stable_nconserv.txt", 
                        ci_stable['nconserv'], fmt="%5i")
